a4f55ef927ebc0d2031e537ea670405ff34bd018
Alexander Mayorov <alexander.majorov@gmail.com>
2022-10-31 17:28:05 +0100

    Refactoring

--- magic_config/magic_config/__init__.py
+++ magic_config/magic_config/__init__.py
@@ -1,5 +1,4 @@
-import sys
-
+# import sys
 # if sys.version_info < (3, 10, 8):
 #     raise ImportError('Your Python version {0} is not supported by MagicConfig, please install '
 #                       'Python 3.10.8+'.format('.'.join(map(str, sys.version_info[:3]))))
