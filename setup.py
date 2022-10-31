d7e89eb5b934eda1e3f954c85c5162b6a8b2a4a4
Alexander Mayorov <alexander.majorov@gmail.com>
2022-10-31 15:37:28 +0100

    Add download_url

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     long_description=get_description(),
     long_description_content_type="text/markdown",
     url='https://github.com/frontdevops/magic-config',
-    # download_url="https://github.com/mike-huls/toolbox_public/archive/refs/tags/0.0.3.tar.gz",
+    download_url="https://github.com/frontdevops/magic-config/archive/refs/tags/0.1.9.tar.gz",
     project_urls={
         "Bug Tracker": "https://github.com/frontdevops/magic-config/issues",
     },
